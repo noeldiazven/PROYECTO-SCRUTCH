33e74337ce11483902899c2c676555cfa3f8fd92
Luis Enrique Murillo Vizcardo <luisito.luis.16.lm@gmail.com>
2015-04-16 23:34:20 -0500

    prueba3

--- prueba.cpp
+++ prueba.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
+typedef int T;
 
 /*MODIFIQUEN ESTE CODIGO PARA PROBAR*/
 
+class prueba{
+private:
+    T valor;
+public:
+    prueba(){valor=0;}
+    void cambiar(T nuevo){valor=nuevo;}
+    void print(){cout<<this->valor<<endl;}
+};
+
 int main(){
-    cout<<"hello world";
+    cout<<"hello world"<<endl;
+    prueba nuevo;
+    nuevo.print();
+    nuevo.cambiar(4);
+    nuevo.print();
     return 0;
     }
