201fd7da909bf0ca51e91bbb70e42724733c725b
Ruben <ruben_jtl@hotmail.com>
2015-04-18 23:09:08 -0500

    smart pointer pero sin template

--- prueba.cpp
+++ prueba.cpp
@@ -15,6 +15,7 @@ public:
     }
     valor sumar_valores(valor,valor);
     void multiplicar_valores(T*);
+    void Display(){ cout<< x<< "," <<y<<endl; }
 };
 // siempre se pone template de nuevo aqui abajo
 template<typename valor>
@@ -25,12 +26,64 @@ template<typename valor>
 void prueba<valor>::multiplicar_valores(T * resultado){
     *resultado=x*y;
 }
+///////////////////////////////////////////////////
+///////////SMART POINTER//////////////////////////
+//////////////////////////////////////////////////
+
+
+//////cree una clase para hacer una prueba//////
+class recta{
+    int x,y;
+public:
+    recta(){x=0; y=0;}
+    recta(int n_x, int n_y){x=n_x; y=n_y;}
+    void display_r(){cout<<x<<","<<y<<endl;}
+};
+
+///////clase smart pointer pero sin template, relacionado con la clase recta//////
+class s_pointer
+
+{
+    recta *m_p;
+public:
+
+
+    s_pointer(recta * new_m_p) : m_p(new_m_p)
+    {
+    }
+    ~s_pointer()
+    {
+        delete m_p;
+    }
+    //////genero sobre carga de operador para que pueda hacer el llamado a display mediante el smart pointer//////
+    recta* operator -> (){
+        return m_p;
+    }
+
+
+
+};
+
+
+
 int main(){
-    prueba <int>uno(5,6);
+   prueba <int>uno(5,6);
+
+    uno.Display();
     cout<<uno.sumar_valores(4,4)<<endl;
     T resultado;
     uno.multiplicar_valores(&resultado);
-    cout<<resultado;
+    cout<<resultado<<endl;
+    //////////////////////////////////////////
+    //////Prueba de punteros inteligentes////
+    ////////////////////////////////////////
+
+    s_pointer r1(new recta());
+    r1->display_r();
+
+
+
+
     return 0;
 }
 // ayque implementar los smart points
